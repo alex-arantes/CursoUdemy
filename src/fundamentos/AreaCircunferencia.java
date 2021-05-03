e80e619000e345438bccb71f5683b3d15122e97a
alex-sander <projetodigital@gmail.com>
2021-05-03 13:57:07 -0300

    Teste alex

---
+++ src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class AreaCircunferencia {
+	
+	public static void main(String[] args) {
+		
+		double raio = 3.4;
+		final double PI = 3.14159; // colocando inicio transforma a variavel em uma constante (não muda o valor)
+		
+		double area = PI * raio * raio; // calculo do raio da transferiencia
+		
+		System.out.println(area);
+	}
+
+}
---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		//Uma sentença de código termina com ;
+		
+		System.out.println("Primeiro Progama #1!!!");
+		/*comentario de várias linhas
+		  linha1
+		  linha2
+		  linha3 
+		 */
+		System.out.println("Primeiro Progama #2!!!");
+		
+	}
+
+}
