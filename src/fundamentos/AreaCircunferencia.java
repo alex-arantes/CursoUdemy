a054143fab2a3a878cd10007c54cce96ab0137a9
alex-sander <projetodigital@gmail.com>
2021-05-10 10:29:04 -0300

    rer

--- src/fundamentos/AreaCircunferencia.java
+++ src/fundamentos/AreaCircunferencia.java
@@ -7,9 +7,13 @@ public class AreaCircunferencia {
 		double raio = 3.4;
 		final double PI = 3.14159; // colocando inicio transforma a variavel em uma constante (não muda o valor)
 		
-		double area = PI * raio * raio; // calculo do raio da transferiencia
+		double area = PI * raio * raio; // calculo do raio da transferencia
 		
 		System.out.println(area);
+		
+		raio = 10;
+		area = PI * raio * raio;
+		System.out.println("Área = " + area + "m2.");
 	}
 
 }
